1936e95c24a56b9dc1dff2823a827214457a41db
kimleb <kimleb64@gmail.com>
2025-10-02 04:34:56 +0300

    pass gen 2

--- python_password_generator.py
+++ python_password_generator.py
@@ -2,7 +2,7 @@ import random
 import string 
 
 def generate_password(length: int=10):
-    alphabet = string.ascii_uppercase + string.digits +string.punctuation
+    alphabet = string.ascii_uppercase +string.ascii_lowercase+ string.digits +string.punctuation
     password = ''.join(random.choice(alphabet) for i in range(length))
     return password
 
